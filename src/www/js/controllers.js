cda5d3640b3192dbea54f54667cf037f1c76b056
Blanca <blanca@nuevebit.com>
2014-04-17 13:12:29 -0500

    Se agregaron los fomularios de registro y solicitud de información

--- src/www/js/controllers.js
+++ src/www/js/controllers.js
@@ -29,10 +29,46 @@ nuevebit.inver.controllers.LoginController = function($scope){
         $scope.ons.screen.presentPage("partials/registro.html");
     }
 }
+nuevebit.inver.controllers.RegistroController = function($scope){
+    $scope.aceptar = function(){
+        $scope.ons.screen.presentPage("partials/home.html");
+    }
+    $scope.cancelar = function(){
+        $scope.ons.screen.presentPage("partials/login.html");
+    }
+}
+
+nuevebit.inver.controllers.PerfilController = function($scope){
+    $scope.aceptar = function(){
+        $scope.ons.screen.presentPage("partials/home.html");
+    }
+    $scope.cancelar = function(){
+        $scope.ons.screen.presentPage("partials/home.html");
+    }
+}
+
+nuevebit.inver.controllers.SolicitudInformacionController = function($scope){
+    $scope.enviar = function(){
+        $scope.ons.screen.presentPage("partials/home.html");
+    }
+    $scope.cancelar = function(){
+        $scope.ons.screen.presentPage("partials/home.html");
+    }
+}
+
+nuevebit.inver.controllers.ListaSolicitudesController = function($scope, SolicitudService){
+    $scope.solicitudes = SolicitudService.getSolicitudes({folio : "solicitudes"});
+}
 
 // registrar controladores con angular
 var inverControllers = angular.module("inverControllers", []);
 inverControllers.controller("MainController",
         ["$scope", nuevebit.inver.controllers.MainController]);
 inverControllers.controller('LoginController', 
-	['$scope', nuevebit.inver.controllers.LoginController])
+	['$scope', nuevebit.inver.controllers.LoginController]);
+inverControllers.controller('RegistroController', 
+    ['$scope', nuevebit.inver.controllers.RegistroController]);
+inverControllers.controller('SolicitudInformacionController', 
+    ['$scope', nuevebit.inver.controllers.SolicitudInformacionController]);
+inverControllers.controller('PerfilController', 
+    ['$scope', nuevebit.inver.controllers.PerfilController]);
--- src/www/template/index.html.tpl
+++ src/www/template/index.html.tpl
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
 <html ng-app="inverApp">
-<head>
-    <meta charset="utf-8" />
-    <meta name="format-detection" content="telephone=no" />
-    <!-- WARNING: for iOS 7, remove the width=device-width and height=device-height attributes. See https://issues.apache.org/jira/browse/CB-4323 -->
-    <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
+	<head>
+		<meta charset="utf-8" />
+		<meta name="format-detection" content="telephone=no" />
+		<!-- WARNING: for iOS 7, remove the width=device-width and height=device-height attributes. See https://issues.apache.org/jira/browse/CB-4323 -->
+		<meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width, height=device-height, target-densitydpi=device-dpi" />
 
-    <link rel='stylesheet' href='bower_components/onsenui/build/css/onsenui.css'>
-    <link rel='stylesheet' href='bower_components/onsenui/build/css/topcoat-mobile-onsen-blue.css'>
+		<link rel='stylesheet' href='bower_components/onsenui/build/css/onsenui.css'>
+		<link rel='stylesheet' href='bower_components/onsenui/build/css/topcoat-mobile-onsen-blue.css'>
 
-    <%= headerInclude %>
-    <% _.forEach(scripts, function(script) { %><script type="text/javascript" src="<%- script %>"></script><% }); %>
+		<%= headerInclude %>
+		<% _.forEach(scripts, function(script) { %><script type="text/javascript" src="<%- script %>"></script><% }); %>
 
-    <title>inVer</title>
+		<title>inVer</title>
 
-</head>
-<body>
-    <ons-screen page="partials/login.html">
-</ons-screen>
-</body>
+	</head>
+	<body>
+		<ons-screen page="partials/login.html"></ons-screen>
+	</body>
 </html>
