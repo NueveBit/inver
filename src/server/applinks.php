e9e7f9abc0f08f8c923e69a4679f5cf80b575baa
Edgar Merino <emerino@nuevebit.com>
2014-08-15 19:13:59 -0500

    agregados metadatos del HTML para applinks para que se muestre la información de cada solicitud adecuadamente desde facebook

--- src/server/applinks.php
+++ src/server/applinks.php
@@ -5,14 +5,33 @@
  * 
  */
 $solicitudId = $_GET["solicitudId"];
+$db = require_once dirname(__FILE__) . "/db.php";
+mysqli_set_charset($db, "utf8");
+
+$sql = "select s.descripcion as descripcion 
+	from SolicitudInformacion as s where s.id=?";
+$stmt = $db->prepare($sql);
+$stmt->bind_param("d", $solicitudId);
+$stmt->execute();
+$stmt->bind_result($descripcion);
+$stmt->fetch();
 ?>
 <html>
     <head>
         <title>Solicitud</title>
-        <meta property="al:android:url" content="inver://solicitud=<?=$solicitudId?>"/>
+        <!-- applinks begin -->
+        <meta property="al:android:url" content="inver://solicitud=<?= $solicitudId ?>"/>
         <meta property="al:android:app_name" content="inVer"/>
         <meta property="al:android:package" content="com.nuevebit.inver"/>
         <meta property="al:web:should_fallback" content="false" />
+        <!-- applinks end -->
+
+        <!-- fb og begin -->
+        <meta property="og:title" content="Solicitud de información : INVER" />
+        <meta property="og:image" content="img/INVER2.png" />
+        <meta property="og:locale" content="es_MX" />
+        <meta property="og:description" content="<?= $descripcion; ?>" />
+        <!-- fb og end -->
     </head>
     <body>
         Para ver esta solicitud, necesitas descargar la aplicación inVer en
